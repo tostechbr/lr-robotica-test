f31e062fb7f88e02b41e9fae14576ed067a9c052
Gustavo <mxgugaa@gmail.com>
2021-04-19 17:01:54 -0300

    Modals JS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
-// Modal JS
+// Primeiro modal (Luciana)
 var modal = document.getElementById("myModal");
-
 var btn = document.getElementById("myBtn");
-
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
@@ -13,8 +11,41 @@ span.onclick = function () {
   modal.style.display = "none";
 }
 
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+// Segundo modal (Julliete)
+var modalJ = document.getElementById("myModal-j");
+var btnJ = document.getElementById("myBtn-j");
+var spanJ = document.getElementsByClassName("close-j")[0];
+
+btnJ.onclick = function () {
+  modalJ.style.display = "block";
+}
+
+spanJ.onclick = function () {
+  modalJ.style.display = "none";
+}
+
+// Terceiro modal (Maria Eduarda)
+var modalM = document.getElementById("myModal-m");
+var btnM = document.getElementById("myBtn-m");
+var spanM = document.getElementsByClassName("close-m")[0];
+
+btnM.onclick = function () {
+  modalM.style.display = "block";
+}
+
+spanM.onclick = function () {
+  modalM.style.display = "none";
+}
+
+// Quarto modal (Gustavo Alessandro)
+var modalG = document.getElementById("myModal-g");
+var btnG = document.getElementById("myBtn-g");
+var spanG = document.getElementsByClassName("close-g")[0];
+
+btnG.onclick = function () {
+  modalG.style.display = "block";
+}
+
+spanG.onclick = function () {
+  modalG.style.display = "none";
 }
