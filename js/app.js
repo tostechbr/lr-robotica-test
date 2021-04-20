165dcaa81bb9c7434e2829bb32dd6ba859865f3d
Tiago Oliveira <tiagolivesan@outlook.com>
2021-04-20 19:41:22 -0300

    Lr Robótica

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+// MENU MOBILE
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+
+  if(toggle && nav){
+      toggle.addEventListener('click', () => {
+          nav.classList.toggle('menu-mobile');
+          toggle.classList.toggle('active-bx');
+      })
+  }
+}
+
+showMenu('bx', 'menu-mobile')
+
 // Primeiro modal (Luciana)
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
